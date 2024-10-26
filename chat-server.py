6ac6abcc64cd2543d3b39fae53c0b3b6fe34efa6
Linielt <danielhlee912@gmail.com>
2024-10-26 16:24:13 +0100

    Added broadcast function and code to handle incoming packets

--- chat-server.py
+++ chat-server.py
@@ -1,43 +1,86 @@
 import json
+import sys
+import socket
+
+import select
 
 
 def usage():
     print("usage: chat-server.py port")
 
-def send_chat_payload(server_socket, nickname, message):
-    chat_packet = b""
+def create_chat_payload(nickname, message):
     chat_payload = {
         "type": "chat",
         "nick": nickname,
         "message": message
     }
-
     chat_bytes = json.dumps(chat_payload).encode("utf-8")
-    chat_len_bytes = len(chat_payload).to_bytes(2, byteorder="big")
-    chat_packet += chat_len_bytes + chat_bytes
+    return chat_bytes
 
-    server_socket.sendall(chat_packet)
-
-def send_join_payload(server_socket, nickname):
-    join_packet = b""
+def create_join_payload(nickname):
     join_payload = {
         "type": "join",
         "nick": nickname
     }
     join_bytes = json.dumps(join_payload).encode("utf-8")
-    join_len_bytes = len(join_payload).to_bytes(2, byteorder="big")
-    join_packet += join_len_bytes + join_bytes
+    return join_bytes
 
-    server_socket.sendall(join_packet)
-
-def send_leave_payload(server_socket, nickname):
-    leave_packet = b""
+def create_leave_payload(nickname):
     leave_payload = {
         "type": "leave",
         "nick": nickname
     }
     leave_bytes = json.dumps(leave_payload).encode("utf-8")
-    leave_len_bytes = len(leave_payload).to_bytes(2, byteorder="big")
-    leave_packet += leave_len_bytes + leave_bytes
+    return leave_bytes
 
-    server_socket.sendall(leave_packet)
+def broadcast(sender_socket, list_of_clients, data):
+    for client in list_of_clients:
+        if client != sender_socket:
+            client.sendall(data.encode("utf-8"))
+
+def run_server(port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('', port))
+    server_socket.listen(5)
+
+    read_set = {server_socket}
+    nicknames = {}
+
+    while True:
+        ready_to_read, _, _ = select.select(read_set, {}, {})
+        for sock in ready_to_read:
+            if sock == server_socket:
+                client_socket, client_address = server_socket.accept()
+                read_set.add(client_socket)
+                print(f"{client_socket.getpeername()}: connected")
+            elif sock != server_socket:
+                data = sock.recv(1024)
+
+                if data:
+                    payload = json.loads(data.decode("utf-8"))
+                    if payload["type"] == "hello":
+                        nicknames[sock] = payload["nick"]
+                        join_payload = create_join_payload(payload["nick"])
+                        broadcast(server_socket, read_set, join_payload)
+                    elif payload["type"] == "chat":
+                        chat_nick = nicknames[sock]
+                        chat_payload = create_chat_payload(chat_nick, payload["message"])
+                        broadcast(server_socket, read_set, chat_payload)
+                elif data is None:
+                    ready_to_read.remove(sock)
+                    read_set.remove(sock)
+                    nicknames.pop(sock)
+                    sock.close()
+                    leave_payload = create_leave_payload(payload["nick"])
+                    broadcast(server_socket, read_set, leave_payload)
+
+
+def main(argv):
+    try:
+        port = int(argv[1])
+    except:
+        usage()
+        sys.exit(1)
+
+
+
